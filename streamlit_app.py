1c246b4af59ceccb93368d46be17ae2dabdaa586
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 04:05:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,6 +67,7 @@ def scatter_plot_df(selected_type = "Trimmed"):
             },
             title="Distribution of Total Price and Total Quantity Purchased"
         )
+        return fig
 
 scatter_plot_df()
 
