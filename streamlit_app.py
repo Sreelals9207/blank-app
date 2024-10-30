69457b0cef259f02d5ee0d33b3a3b5ef868152c4
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 03:51:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.title("Clustering of Retail shop customers.")
-#selected_type = st.radio("Choose data type:", ('Trimmed', 'Non-Trimmed'))
+selected_type = st.radio("Choose data type:", ('Trimmed', 'Non-Trimmed'))
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
