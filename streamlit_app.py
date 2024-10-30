6a2cef4d82a08b330203c1c8b6c6091ca86564f6
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 03:34:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
 
 st.title("Clustering of Retail shop customers.")
+selected_type = st.radio("Choose data type:", ('Trimmed', 'Non-Trimmed'))
+
+
+
+
+
+
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
