4b2d1180a727041ff594ace9d8d1e33f09857c16
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 03:16:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Clustering model")
+st.title("Clustering of Retail shop customers")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
