a2b7b7ea802056881741b58971850e2f61242cb6
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 09:14:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,6 +69,48 @@ st.header(
     f"shape of the dataframe is{shape}."
 )
 
+def for_df(selected_type = "Trimmed"):
+    data = pd.read_csv("OnlineRetail.csv.zip", encoding='ISO-8859-1')
+
+    #making the data into a dataframe
+    df = pd.DataFrame(data)
+
+    #implimenting a new column 'totalprize'
+    df["totalprize"] = df["Quantity"] * df["UnitPrice"]
+
+    #calculating the total money spend by each customer
+    df1 = df.groupby("CustomerID")["totalprize"].sum().reset_index()
+
+    #calculating the total quandity purchased by each customer
+    df2 = df.groupby("CustomerID")["Quantity"].sum().reset_index()
+
+    #combainning 'df1' and 'df2' in a single dataframe
+    df = pd.merge(df1, df2, on='CustomerID', how='outer')
+
+    #rename the column 'Quandity'
+    df.rename(columns={"Quantity" : "totalquandity"}, inplace=True)
+    
+    if selected_type == 'Non-Trimmed':
+        df.set_index("CustomerID", inplace=True)
+        df1 = df[["totalprize", "totalquandity"]]
+    else:
+        df.set_index("CustomerID", inplace=True)
+                #trimmed 10% bottom and top of 'df'.
+        lower_bound_col1 = df["totalprize"].quantile(0.02)
+        upper_bound_col1 = df["totalprize"].quantile(0.98)
+        
+        lower_bound_col2 = df["totalquandity"].quantile(0.02)
+        upper_bound_col2 = df["totalquandity"].quantile(0.98)
+        
+        df1 = df[
+            (df["totalprize"] >= lower_bound_col1) & (df["totalprize"] <= upper_bound_col1) &
+            (df["totalquandity"] >= lower_bound_col2) & (df["totalquandity"] <= upper_bound_col2)
+        ]
+    return df1
+
+
+
+
 #def k_cluster(selected_type="Trimmed", k = 2):
     #if selected_type == "Trimmed":
         
