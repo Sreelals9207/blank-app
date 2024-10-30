84de6721609fc01b80f15d9e15a649bc391679ab
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 05:14:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,8 +65,7 @@ def scatter_plot_df(selected_type = "Trimmed"):
 fig , shape = scatter_plot_df(selected_type)
 st.plotly_chart(fig)
 st.title(
-    f"shape of the dataframe is
-    rows= {shape[0]}"
+    f"shape of the dataframe is{shape}."
 )
 
 
