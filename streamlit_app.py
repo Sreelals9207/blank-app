c4280edb9bb3f7beebbed602ca6cb66318fe737e
Sreelal sasilal <sreelals9207@gmail.com>
2024-10-30 22:33:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,6 +129,4 @@ st.subheader(f"silhouette_score for the model : {ss}.")
 
 
 
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
